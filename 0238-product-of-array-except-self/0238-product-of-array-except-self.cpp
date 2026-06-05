2cec3d78cb9aaefca5c2fd500c71ba7d30a104ed
sun <suryanshkamal7@gmail.com>
2026-06-05 11:03:58 +0530

    Time: 0 ms (100%), Space: 41.1 MB (41.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,19 +3,16 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int x=nums.size();
         vector<int> answer(x);
-        vector<int> prefix(x);
-        vector<int> suffix(x);        
-        prefix[0]=1;
+        vector<int> suffix(x);
         suffix[x-1]=1;
+        answer[0]=1;
         for(int i=1;i<x;i++){
-            prefix[i]=prefix[i-1]*nums[i-1];
+            answer[i]=answer[i-1]*nums[i-1];
         }
         for(int j=x-2;j>=0;j--){
             suffix[j]=suffix[j+1]*nums[j+1];
+            answer[j]*=suffix[j];
         }
-        for(int i=0;i<x;i++){
-            answer[i]=prefix[i]*suffix[i];
-        } 
         return answer;       
     }
 };
